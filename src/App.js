ba022913ed7c64c6e7e7f04d064c51cbf7f505ea
rob405 <rob405gamedev@gmail.com>
2023-09-14 18:45:13 +0300

    made basic router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Page1 />} />
+        <Route path='/2' element={<Page2 />} />
+      </Routes>
+    </>
   );
 }
 
+const Page1 = () => (
+  <h1>1</h1>
+)
+
+const Page2 = () => (
+  <h1>2</h1>
+)
 export default App;
